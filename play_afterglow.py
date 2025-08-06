196b83fd5d9cd1589539a30d1d5dfe97a2a35afc
Yogesh Wagh <ywagh4430@gmail.com>
2025-08-06 23:01:55 +0530

    restarted everything, can plot a flux variation for single frequency

--- play_afterglow.py
+++ play_afterglow.py
@@ -1,12 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, Button, RadioButtons, CheckButtons
 import afterglowpy as grb
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Button, Slider
 
-fig, ax = plt.subplots(figsize=(12, 12))
-plt.subplots_adjust(left=0.25, bottom=0.4, right=0.75)
-
-time = np.geomspace(1e1, 1e6, 30)
+time = np.linspace(1e1,1e6,30)
 
 bands = {
     'r': 485448262349447.2,
@@ -14,11 +11,14 @@ bands = {
     'i': 400258022050793.1,
     'R': 468671768303359.1,
     'I': 378163424193228.9,
-    'TKEV': 12.417990504024E+18,
-    'RA': 1254600000000
+    'V': 556239404189155.8,
+    'B': 684457666666666.5,
+    'z': 328443179329975.9,
+    '10keV': 2.417990504024E+18,
+    '1254.6 GHz': 1254600000000,
+    '647.8 GHz': 647800000000
 }
-band_lines = {}
-#initial params
+
 params = {
     'jetType': grb.jet.TopHat,
     'specType': 0,
@@ -36,80 +36,26 @@ params = {
     'z': 0.5
 }
 
-def plot_bands():
-    ax.clear()
-    for band, freq in bands.items():
-        if check.get_status()[list(bands).index(band)]:
-            Fnu = grb.fluxDensity(time, np.full_like(time, freq), **params)
-            line, = ax.loglog(time, Fnu, label=f'{band}-band (ν={freq:.1e} Hz)')
-            band_lines[band] = line
-    ax.legend()
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Flux density (mJy)')
-    fig.canvas.draw_idle()
+# slider_defs = [
+#     ('thetaObs', 0.0, 0.8, params['thetaObs']),
+#     ('log10_E0', 50.0, 60.0, np.log10(params['E0'])),
+#     ('thetaCore', 0.01, 0.8, params['thetaCore']),
+#     ('thetaWing', params['thetaCore']*1.1, 2.0, params['thetaWing']),
+#     ('b', 0.0, 10.0, params['b']),
+#     ('n0', 1e-5, 1.0, params['n0']),
+#     ('p', 2.0, 3.0, params['p']),
+#     ('epsilon_e', 1e-3, 0.5, params['epsilon_e']),
+#     ('epsilon_B', 1e-6, 0.1, params['epsilon_B']),
+#     ('xi_N', 0.1, 1.0, params['xi_N']),
+#     ('z', 0.01, 5.0, params['z'])
+# ]
 
-slider_defs = [
-    ('thetaObs', 0.0, 0.3, params['thetaObs']),
-    ('log10_E0', 45.0, 60.0, np.log10(params['E0'])),
-    ('thetaCore', 0.001, 0.3, params['thetaCore']),
-    ('thetaWing', params['thetaCore']*1.1, 2.0, params['thetaWing']),
-    ('b', 0.0, 10.0, params['b']),
-    ('n0', 1e-5, 1.0, params['n0']),
-    ('p', 2.0, 3.0, params['p']),
-    ('epsilon_e', 1e-3, 0.5, params['epsilon_e']),
-    ('epsilon_B', 1e-6, 0.1, params['epsilon_B']),
-    ('xi_N', 0.1, 1.0, params['xi_N']),
-    ('z', 0.01, 10.0, params['z'])
-]
-sliders = {}
-for i, (key, vmin, vmax, init) in enumerate(slider_defs):
-    axsl = plt.axes([0.25, 0.30 - i*0.025, 0.65, 0.02])
-    sliders[key] = Slider(axsl, key, valmin=vmin, valmax=vmax, valinit=init)
+# ('TopHat', 'Gaussian', 'PowerLawCore', 'GaussianCore', 'Spherical', 'PowerLaw')
+nu  = bands['r']
 
-rax = plt.axes([0.8, 0.65, 0.15, 0.25])
-radio = RadioButtons(rax, ('TopHat', 'Gaussian', 'PowerLawCore', 'GaussianCore', 'Spherical', 'PowerLaw'))
-jet_map = {
-    'TopHat': grb.jet.TopHat,
-    'Gaussian': grb.jet.Gaussian,
-    'PowerLawCore': grb.jet.PowerLawCore,
-    'GaussianCore': grb.jet.GaussianCore,
-    'Spherical': grb.jet.Spherical,
-    'PowerLaw': grb.jet.PowerLaw
-}
-
-cax = plt.axes([0.8, 0.45, 0.15, 0.2])
-check = CheckButtons(cax, list(bands.keys()), [True]*len(bands))
-
-def update(val):
-    for key, slider in sliders.items():
-        if key == 'log10_E0':
-            params['E0'] = 10 ** slider.val
-        else:
-            params[key] = slider.val
-    plot_bands()
-
-for slider in sliders.values():
-    slider.on_changed(update)
-
-def on_jet(label):
-    params['jetType'] = jet_map[label]
-    update(None)
-radio.on_clicked(on_jet)
-
-def on_check(label):
-    update(None)
-check.on_clicked(on_check)
-
-resetax = plt.axes([0.8, 0.33, 0.1, 0.04])
-button = Button(resetax, 'Reset', hovercolor='skyblue')
-def reset(event):
-    for key, slider in sliders.items():
-        slider.reset()
-    radio.set_active(0)
-    for _, chk in enumerate(check.get_rectangles()):
-        pass
-    update(None)
-button.on_clicked(reset)
-
-plot_bands()
+flux = grb.fluxDensity(time, nu, **params)
+fig, ax = plt.subplots(figsize = (8,6))
+fig.subplots_adjust(left=0.05, bottom=0.25)
+ax.loglog(time, flux)
+ax.grid(True, linestyle = '-', linewidth = 0.3, which = 'both')
 plt.show()
